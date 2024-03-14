ac94187818788f2f09e14682a84c70b7fd39f1b3
RavichandraBK <ravichandrak2707@gmail.com>
2024-03-14 17:12:04 +0530

    Updated client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,17 @@
-import LoginPage from './Pages/LoginPage'
-import {  Route, Routes } from 'react-router-dom'
-import RegisterPage from './Pages/RegisterPage'
-function App(){
-  // const [count, setCount] = useState(0)
+import LoginPage from './Pages/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import RegisterPage from './Pages/RegisterPage';
+import React from 'react';
 
+function App(): JSX.Element {
   return (
     <> 
-   <Routes>
-   <Route path='/' element={<LoginPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-   </Routes>
-      
-     
-   
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
