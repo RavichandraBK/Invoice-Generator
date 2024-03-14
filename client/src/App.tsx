2d85b68dd4bf821e3a47cbbd4051d0bccfcfbf6d
RavichandraBK <ravichandrak2707@gmail.com>
2024-03-14 16:55:41 +0530

    Updated client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import LoginPage from './Pages/LoginPage'
-import { Router, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 import RegisterPage from './Pages/RegisterPage'
 function App(){
   // const [count, setCount] = useState(0)
