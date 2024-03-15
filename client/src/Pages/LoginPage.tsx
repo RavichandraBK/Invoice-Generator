ce311324f2234bede66c8963a8b82c69e3834346
RavichandraBK <ravichandrak2707@gmail.com>
2024-03-15 17:56:44 +0530

    Updated client

--- client/src/Pages/LoginPage.tsx
+++ client/src/Pages/LoginPage.tsx
@@ -37,7 +37,7 @@ const LoginPage: React.FC = () => {
       }
       else{
         toast.success(userLogin.data.message)
-        Navigate('/Register')
+        Navigate('/AddProduct')
       }
       
     }
